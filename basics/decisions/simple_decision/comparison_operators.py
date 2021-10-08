a458db008e2bb4dede587a7628fcffaf50433018
Lily Kettle <5kettl01@solent.ac.uk>
2021-10-08 15:40:53 +0100

    Added code to demonstrate the use of comparison operators.

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,10 @@
+first_number = int(input("enter a whole number"))
+second_number = int(input("enter another whole number"))
+if first_number > second_number:
+    print(first_number , "is bigger than" , second_number)
+
+elif first_number < second_number:
+    print(second_number , "is bigger than" , first_number)
+
+else:
+    print(first_number , "is equal to" , second_number)
