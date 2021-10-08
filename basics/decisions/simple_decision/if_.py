13dff0538e9daaaa9dd293496aa64955a75f40af
Lily Kettle <5kettl01@solent.ac.uk>
2021-10-08 14:45:57 +0100

    Added code to demonstrate the use of an if statement.

---
+++ basics/decisions/simple_decision/if_.py
@@ -0,0 +1,19 @@
+genre=str(input("what type of genre do you like?"))
+if genre == "thriller":
+    print("Do you like thriller books?")
+    print("Finished reading book!")
+
+
+elif genre == "action":
+    print("Do you like action books?")
+    print("Finished reading book!")
+
+
+elif genre == "horror":
+    print("Do you like horror books?")
+    print("Finished reading book!")
+
+
+else:
+    print("you like" , genre, "books")
+    print("finished reading book")
